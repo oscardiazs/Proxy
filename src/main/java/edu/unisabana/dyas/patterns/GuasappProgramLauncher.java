cf06908d5443e93c3a44aaf65b61fa367feaa19e
oscardiazs <diazsanchezoscar@gmail.com>
2024-06-08 21:36:07 -0400

    Primer

--- src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
+++ src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
@@ -3,7 +3,7 @@ package edu.unisabana.dyas.patterns;
 // GuasappProgramLauncher.java
 import java.util.Objects;
 
-import main.java.edu.unisabana.dyas.patterns.util.MessagingClient;
+import edu.unisabana.dyas.patterns.util.MessagingClient;
 
 public class GuasappProgramLauncher {
     public static void main(String[] args) {
