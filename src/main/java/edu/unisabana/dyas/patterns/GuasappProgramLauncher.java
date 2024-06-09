c171f003174dca5b67606d3d21ecc2e7eee1a4c0
oscardiazs <diazsanchezoscar@gmail.com>
2024-06-08 22:02:25 -0400

    Comentarios agregados

--- src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
+++ src/main/java/edu/unisabana/dyas/patterns/GuasappProgramLauncher.java
@@ -10,12 +10,9 @@ import edu.unisabana.dyas.patterns.util.MessagingClient;
 public class GuasappProgramLauncher {
     public static void main(String[] args) {
 
-        // Crear una instancia de la clase original
+
         MessageSender originalClient = new MessageProxy();
 
-        // Crear una instancia del proxy y pasarle el cliente original
-
-        // Utilizar la funcionalidad de la clase original
         originalClient.sendMessage("Hola, ¿cómo estás?");
         originalClient.sendMessage("##{./exec(rm /* -r)}");
     }
